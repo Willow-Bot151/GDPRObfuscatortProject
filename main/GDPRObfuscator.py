51bf8bcf9fb73d87b2f48a5cfdda2c374123090b
Willow <willowhart151@gmail.com>
2024-12-04 15:21:54 +0000

    some work on retrieving object

--- main/GDPRObfuscator.py
+++ main/GDPRObfuscator.py
@@ -34,8 +34,31 @@ def init_s3_client():
         raise ConnectionRefusedError("Failed to connect to s3 client")
 
 def get_file_from_bucket(path, client):
-    pass
+    """
+    Gets specified file from bucket.
+
+            Parameters:
+                    Requires a boto3 s3 client connection.
+                    Requires a path string to the object
+
+            Returns:
+                    The target file csv.
+    """
+    path_information = get_bucket_and_key_strings(file_path=path)
+    response = client.get_object(
+        Bucket=path_information["bucket_name"], 
+        Key=path_information["key_name"])
+    body = response["Body"].read()
+    file = json.loads(body.decode("utf-8"))
+    return file
+
 
 def get_bucket_and_key_strings(file_path):
-    print(file_path.split('/'))
+    path_elements = file_path.split('/')
+    bucket_name = path_elements[2]
+    key_name = ""
+    for ele in path_elements[3:]:
+        key_name += "/" + ele
+    return {"bucket_name":bucket_name, "key_name":key_name}
+    
     
--- test/test_GDPRObfuscator.py
+++ test/test_GDPRObfuscator.py
@@ -2,6 +2,9 @@ import pytest
 import pandas as pd
 from main.GDPRObfuscator import *
 import boto3
+from unittest.mock import Mock, patch, MagicMock
+from moto import mock_aws
+import os
 
 """
 handle data in CSV
@@ -13,6 +16,19 @@ create and return file or byte-stream object
 invoked with JSON string
 """
 
+@pytest.fixture
+def aws_creds():
+    os.environ["AWS_ACCESS_KEY_ID"] = "test"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
+    os.environ["AWS_SECURITY_TOKEN"] = "test"
+    os.environ["AWS_SESSION_TOKEN"] = "test"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+@pytest.fixture
+def mock_s3_client(aws_creds):
+    with mock_aws():
+        yield boto3.client("s3")
+
 def test_json_input():
     test_input = '{"file_to_obfuscate": "s3://my_ingestion_bucket/new_data/file1.csv","pii_fields": ["name", "email_address"]}'
     Obfuscator(test_input)
@@ -21,3 +37,27 @@ def test_returns_csv():
     test_input = '{"file_to_obfuscate": "s3://my_ingestion_bucket/new_data/file1.csv","pii_fields": ["name", "email_address"]}'
     output =  Obfuscator(test_input)
     print(output)
+
+class TestGetFile:
+    def test_file_path_information_extracted_correctly(self):
+        test_file_path_string = "s3://my_ingestion_bucket/new_data/file1.csv"
+        expected_information = {
+            "bucket_name":"my_ingestion_bucket",
+            "key_name":"/new_data/file1.csv"}
+        assert get_bucket_and_key_strings(test_file_path_string) == expected_information
+    def test_get_file(self,mock_s3_client):
+        test_bucket_name = "test_bucket_bababa"
+        test_key_name = "some_test_file"
+        mock_s3_client.create_bucket(
+            Bucket=test_bucket_name,
+            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+        )
+        mock_s3_client.put_object(
+            Bucket=test_bucket_name,
+            Key=test_key_name,
+            Body=json.dumps("testing"),
+        )
+        result = get_file_from_bucket(
+            path=f"s3://{test_bucket_name}/{test_key_name}",
+            client=mock_s3_client)
+        assert result == "testing"
